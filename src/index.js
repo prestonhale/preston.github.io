46e2f230aedd57e0624cee307f4abbe0a77ed323
Preston <slethik@gmail.com>
2018-01-15 23:11:32 -0500

    Add logo

--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@
 $(document).ready(function() {
     var handlebars = require('handlebars');  // Templating
 
-    console.log("Start");
-    var greatBearSource   = document.getElementById("greatBear-template").innerHTML;
+    var greatBearSource = document.getElementById("greatBear-template").innerHTML;
     var greatBearTemplate = handlebars.compile(greatBearSource);
 
-    // var keepItGroovySource   = document.getElementById("keepItGroovy-template").innerHTML;
-    // var keepItGroovyTemplate = Handlebars.compile(keepItGroovySource);
+    var keepItGroovySource = document.getElementById("keepItGroovy-template").innerHTML;
+    var keepItGroovyTemplate = handlebars.compile(keepItGroovySource);
 
-    // var priestSimulatorSource   = document.getElementById("priestSimulator-template").innerHTML;
-    // var priestSimulatorTemplate = Handlebars.compile(priestSimulatorSource);
+    var priestSimulatorSource = document.getElementById("priestSimulator-template").innerHTML;
+    var priestSimulatorTemplate = handlebars.compile(priestSimulatorSource);
 
+
+    $('.project-box').on('click', function() {
+        var project = this.dataset.project;
+        var template = "";
+
+        $(this).siblings().each(function(i, elem){
+            $(elem).removeClass('selected');
+        })
+        $(this).addClass("selected");
+
+        switch(project){
+            case 'greatBear':
+                template = greatBearTemplate({});
+                break;
+            case 'keepItGroovy':
+                template = keepItGroovyTemplate({})
+                break;
+            case 'priestSimulator':
+                template = priestSimulatorTemplate({})
+                break;
+        }
+        document.getElementById('project-viewer').innerHTML = template;
+    })
+    
     document.getElementById('project-viewer').innerHTML = greatBearTemplate({});
-
-    console.log("Done");
 });
