1f47df81472a546e90ae95764945afc50729c698
root <root@DESKTOP-9NO76M1.localdomain>
2018-09-06 22:53:54 -0400

    More formatting

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var projectData;
+var projectNames
 var handlebars;
 var index = 0;
 
 $(document).ready(function() {
     handlebars = require('handlebars');  // Templating
+
+    handlebars.registerHelper('carousel-li', function(curIndex, activeIndex){
+        console.log(curIndex + "==" + activeIndex)
+        if (curIndex == activeIndex){
+            return new handlebars.SafeString(
+                '<li class="active"></li>'
+            )
+        }
+        return new handlebars.SafeString(
+            '<li></li>'
+        )
+    })
+
     getProjectData();
 });
 
@@ -14,6 +28,7 @@ function getProjectData() {
         mimeType: "application/json",
         success: function(data) {
             projectData = data;
+            projectNames = Object.keys(projectData);
             displayProject('greatBear');
             bindProjectDisplayEvents();
         }
@@ -34,7 +49,6 @@ function bindProjectDisplayEvents(){
 }
 
 function bindProjectCarouselEvents(){
-    let projectNames = Object.keys(projectData);
     $('.project-control-next').on('click', function() {
         index = (index + 1) % projectNames.length;
         displayProject(projectNames[index]);
@@ -55,6 +69,8 @@ function displayProject(projectName){
     $.extend(true, templateData, project);
     templateData.description = templateData.description.reduce(function(x, y){return x + y;});
     templateData.commentary = templateData.commentary.reduce(function(x, y){return x + y;});
+    templateData.project_names = projectNames;
+    templateData.index = index;
 
     var projectSource = document.getElementById("project-template").innerHTML;
     var projectTemplate = handlebars.compile(projectSource);
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,24 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var projectData;
+var projectNames
 var handlebars;
 var index = 0;
 
 $(document).ready(function() {
     handlebars = require('handlebars');  // Templating
+
+    handlebars.registerHelper('carousel-li', function(curIndex, activeIndex){
+        console.log(curIndex + "==" + activeIndex)
+        if (curIndex == activeIndex){
+            return new handlebars.SafeString(
+                '<li class="active"></li>'
+            )
+        }
+        return new handlebars.SafeString(
+            '<li></li>'
+        )
+    })
+
     getProjectData();
 });
 
@@ -15,6 +29,7 @@ function getProjectData() {
         mimeType: "application/json",
         success: function(data) {
             projectData = data;
+            projectNames = Object.keys(projectData);
             displayProject('greatBear');
             bindProjectDisplayEvents();
         }
@@ -35,7 +50,6 @@ function bindProjectDisplayEvents(){
 }
 
 function bindProjectCarouselEvents(){
-    let projectNames = Object.keys(projectData);
     $('.project-control-next').on('click', function() {
         index = (index + 1) % projectNames.length;
         displayProject(projectNames[index]);
@@ -56,6 +70,8 @@ function displayProject(projectName){
     $.extend(true, templateData, project);
     templateData.description = templateData.description.reduce(function(x, y){return x + y;});
     templateData.commentary = templateData.commentary.reduce(function(x, y){return x + y;});
+    templateData.project_names = projectNames;
+    templateData.index = index;
 
     var projectSource = document.getElementById("project-template").innerHTML;
     var projectTemplate = handlebars.compile(projectSource);
